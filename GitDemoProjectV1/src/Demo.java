596a711c5866d67da265eac2557492e3bcfab67f
Rikhi9812 <107738848+Rikhi9812@users.noreply.github.com>
2022-06-18 14:02:34 +0900

    MeRana

--- GitDemoProjectV1/src/Demo.java
+++ GitDemoProjectV1/src/Demo.java
@@ -7,6 +7,10 @@ public class Demo {
 		System.out.println("Hello world");
 		System.out.println("Hello world");
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		
 
 	}
 
