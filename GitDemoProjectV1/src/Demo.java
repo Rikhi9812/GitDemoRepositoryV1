b2d630f7b179c454f0381ff5e7323636eb3f5b82
Rikhi-Rana <96675139+Rikhi-Rana@users.noreply.github.com>
2022-06-19 03:56:05 +0900

    23456

--- GitDemoProjectV1/src/Demo.java
+++ GitDemoProjectV1/src/Demo.java
@@ -4,9 +4,9 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
+		System.out.println("Hello world1");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 		System.out.println("Hello world");
 		System.out.println("Hello world");
 		System.out.println("Hello world");
