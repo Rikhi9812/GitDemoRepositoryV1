add65d7577c8ad6b7ea90a8bd5bd42d5d888abf1
RANA RIKHI RAM <RANA RIKHI RAM@192.168.0.102>
2022-06-18 17:33:16 +0900

    123

--- GitDemoProjectV1/src/Rana.java
+++ GitDemoProjectV1/src/Rana.java
@@ -1,7 +1,45 @@
  public class Rana {
   
-  private int idj;
+
+  
+  @Override
+	public String toString() {
+		return "Rana [idj=" + idj + ", name=" + name + ", emailId=" + emailId + ", mail=" + mail + "]";
+	}
+public int getIdj() {
+		return idj;
+	}
+	public void setIdj(int idj) {
+		this.idj = idj;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+private int idj;
   private String name;
   private String emailId;
+  private String mail;
+  
+  
+  
+  
+  public void rana() {
+   System.out.println("Hello rana");
+  }
   
 }
