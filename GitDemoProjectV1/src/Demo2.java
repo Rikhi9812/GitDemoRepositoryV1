42c25124a28b194a66419b893aa85a9eabb8348c
Rikhi-Rana <96675139+Rikhi-Rana@users.noreply.github.com>
2022-06-18 16:50:03 +0900

    Update Demo2.java

--- GitDemoProjectV1/src/Demo2.java
+++ GitDemoProjectV1/src/Demo2.java
@@ -5,14 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int data[] = {1, 3, 5, 7};
-		for(int i : data) {
-			System.out.println(data[i]);
-		}
-		System.out.println("i");
-		System.out.println("i");
-		System.out.println("i");
-		System.out.println("i");
+		
 
 	}
 
