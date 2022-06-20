17580af6be1c7dca3a6058ca9f7df38ba3dd0b43
Rikhi-Rana <96675139+Rikhi-Rana@users.noreply.github.com>
2022-06-20 12:42:35 +0900

    Update Demo2.java

--- GitDemoProjectV1/src/Demo2.java
+++ GitDemoProjectV1/src/Demo2.java
@@ -14,6 +14,19 @@ public class Demo2 {
 
 		
 		System.out.println("Hello world2345");
+		
+		
+		System.out.println("Hello world1");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
+		System.out.println("Hello world4");
+		System.out.println("Hello world5");
+		System.out.println("Hello world6");
+		
+		
+		for(int i = 0; i <= 5; i++){
+			System.out.println(i);
+		}
 
 		
 
