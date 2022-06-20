b2007219326a198aec34f1465d940ae2df5ec01a
RANA RIKHI RAM <RANA RIKHI RAM@192.168.0.102>
2022-06-20 12:33:52 +0900

    Rana commit

--- GitDemoProjectV1/src/Rana.java
+++ GitDemoProjectV1/src/Rana.java
@@ -1,1 +1,31 @@
-
+public class Rana {
+	
+	private int id;
+	private String name;
+	private String mail;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	@Override
+	public String toString() {
+		return "Rana [id=" + id + ", name=" + name + ", mail=" + mail + "]";
+	}
+	
+	
+	
+}
